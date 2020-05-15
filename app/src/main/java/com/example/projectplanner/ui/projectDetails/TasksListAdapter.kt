7ba4db86ddd245074a07196056a795c29fab4a0b
Rozaliya <rozaliya.amirova@gmail.com>
2020-05-15 22:46:15 +0300

    fixed layout in project details

--- app/src/main/java/com/example/projectplanner/ui/projectDetails/TasksListAdapter.kt
+++ app/src/main/java/com/example/projectplanner/ui/projectDetails/TasksListAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.projectplanner.ui.projectDetails
 
 import android.content.Context
-import android.provider.Settings.Global.getString
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +8,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projectplanner.R
 import com.example.projectplanner.data.db.models.Task
+import java.text.SimpleDateFormat
+import java.util.*
 
 class TasksListAdapter internal constructor(val context: Context) : RecyclerView.Adapter<TasksListAdapter.TaskViewHolder>() {
 
@@ -27,8 +28,17 @@ class TasksListAdapter internal constructor(val context: Context) : RecyclerView
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
         val current = animals[position]
+
+        val startDate = toString(current.taskStartDate)
+        val endDate = toString(current.taskEndDate)
+
         holder.taskName.text = current.taskTitle
-        holder.taskDate.text = String.format(context.getString(R.string.task_date_string), current.taskStartDate.toString(), current.taskEndDate.toString())
+        holder.taskDate.text = String.format(context.getString(R.string.task_date_string), startDate, endDate)
+    }
+
+    private fun toString(date: Date): String {
+        val format = SimpleDateFormat("dd/MM/yy", Locale.US)
+        return format.format(date)
     }
 
     internal fun setTasks(animals: List<Task>) {
